84c8d62b01347857e32332e4cedcbfbbbb00c1bc
Drake Axelrod <drake@draxel.io>
2022-03-30 21:07:22 +0200

    Add cross-platform notifications lib
    library -> github.com/gen2brain/beep

---
+++ cmd/demo.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"github.com/gen2brain/beeep"
+	"github.com/spf13/cobra"
+)
+
+// demoCmd represents the demo command
+var demoCmd = &cobra.Command{
+	Use:   "demo",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := beeep.Notify("Title", "Message body", "assets/information.png")
+		if err != nil {
+				panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(demoCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// demoCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// demoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
